f74eeaeb021454bb9fccd737a96e2f976a7da404
PhuongNV <vanphuong.nguyen@rikai.technology>
2021-10-19 17:33:09 +0700

    demo module user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModuleModule } from './user-module/user-module.module';
-
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserModuleModule } from "./user-module/user-module.module";
 @Module({
-  imports: [UserModuleModule],
-  controllers: [AppController],
-  providers: [AppService],
+ imports: [UserModuleModule],
+ controllers: [AppController],
+ providers: [AppService],
 })
 export class AppModule {}
---
+++ src/user-module/user-controller/user-controller.controller.ts
@@ -0,0 +1,11 @@
+// import { Controller } from '@nestjs/common';
+import { Controller,Get } from '@nestjs/common';
+import {UserService} from '../user-service';
+@Controller('user-controller')
+export class UserControllerController {
+    constructor(private readonly service:UserService){}
+    @Get()
+    getUser(){
+     return this.service.getUser()
+    }
+}
